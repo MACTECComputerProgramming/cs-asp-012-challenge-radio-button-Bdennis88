570a3229b8340241457eafc45770603ef71f8122
Dalton Teter <21dteter@macon.k12.mo.us>
2020-10-08 12:57:12 -0500

    CS_ASP_017 Challange

---
+++ CS_ASP_016/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CS_ASP_016
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Instantiate tow Date Time variables
+
+        public DateTime myFirstDate, mySecondDate;
+
+
+
+        // Get First Date                       
+        public void myCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            myFirstDate = myCalendar.SelectedDate.Value;
+            
+        }
+
+        // Get my Second Date
+
+        public void mySecondCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            mySecondDate = mySecondCalendar.SelectedDate.Value;
+
+        }
+
+        // Call Black Out Method
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+
+
+        }
+
+
+
+
+    }
+}
